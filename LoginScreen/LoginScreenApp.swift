9d92e34b676ee0a58fb4d9148478a46054fd7bda
Igor Buzykin <lexemz@gmail.com>
2021-11-06 19:33:21 +0300

    Create UserManager

--- LoginScreen/LoginScreenApp.swift
+++ LoginScreen/LoginScreenApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct LoginScreenApp: App {
+    @StateObject private var user = UserManager()
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            ContentView()
+            RegisterView()
         }
     }
 }
---
+++ LoginScreen/UserManager.swift
@@ -0,0 +1,12 @@
+//
+//  UserManager.swift
+//  LoginScreen
+//
+//  Created by Igor on 06.11.2021.
+//
+
+import Combine
+
+class UserManager: ObservableObject {
+    var userName = ""
+}
